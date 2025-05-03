5fee12d39a608a374f5cf82dc3934b89cfa0d97f
EliDaher <eeli56315@gmail.com>
2025-05-03 10:08:49 +0300

    add filePond

--- src/pages/AddExercise.tsx
+++ src/pages/AddExercise.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+// FilePond
+import { FilePond, registerPlugin } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+registerPlugin(FilePondPluginImagePreview, FilePondPluginFileValidateType);
+
 export default function Exercise() {
   const [exerciseName, setExerciseName] = useState('');
   const [category, setCategory] = useState('');
@@ -16,15 +25,15 @@ export default function Exercise() {
     { value: 'Advanced', label: 'Advanced' },
   ];
 
-  const clearData = () =>{
-    setExerciseName('')
+  const clearData = () => {
+    setExerciseName('');
     setCategory('');
     setBodyPart('');
-    setDifficulty('')
-    setDescription('')
-    setCommonMistakes('')
-    setImageFile(null)
-  }
+    setDifficulty('');
+    setDescription('');
+    setCommonMistakes('');
+    setImageFile(null);
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -52,7 +61,7 @@ export default function Exercise() {
 
       console.log('✅ Success:', response.data);
       alert('تم حفظ التمرين بنجاح');
-      clearData()
+      clearData();
     } catch (error: any) {
       console.error('❌ Error:', error.response?.data || error.message);
       alert('حدث خطأ أثناء حفظ التمرين');
@@ -61,114 +70,97 @@ export default function Exercise() {
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-black via-gray-900 to-gray-800 text-white flex flex-col">
+      <h2 className="text-4xl font-bold text-white text-center mb-8 mt-3 tracking-tight">Welcome</h2>
 
-        <h2 className="text-4xl font-bold text-white text-center mb-8 mt-3 tracking-tight">Welcome</h2>
-        
-        <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-[80%] mx-auto">
-            <div className="flex flex-col">
-              <label className="text-sm text-white mb-2 ml-3">Exercise Name</label>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-[80%] mx-auto">
+        <div className="flex flex-col">
+          <label className="text-sm text-white mb-2 ml-3">Exercise Name</label>
+          <input
+            type="text"
+            className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
+            placeholder="Push-Up"
+            value={exerciseName}
+            onChange={(e) => setExerciseName(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label className="text-sm text-white mb-2 ml-3">Category</label>
+          <input
+            type="text"
+            className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
+            placeholder="Strength, Cardio, ..."
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-col">
+          <label className="text-sm text-white mb-2 ml-3">Body part</label>
+          <input
+            type="text"
+            className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
+            placeholder="Chest, Legs, ..."
+            value={bodyPart}
+            onChange={(e) => setBodyPart(e.target.value)}
+          />
+        </div>
+
+        <div className="flex items-center justify-around">
+          {difficultyList.map((item) => (
+            <div className="flex flex-row gap-2" key={item.value}>
               <input
-                type="text"
-                className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
-                placeholder="Push-Up"
-                value={exerciseName}
-                onChange={(e) => {setExerciseName(e.target.value)}}
-                required
+                name="difficulty"
+                type="radio"
+                value={item.value}
+                id={item.value}
+                checked={difficulty === item.value}
+                onChange={(e) => setDifficulty(e.target.value)}
               />
+              <label htmlFor={item.value}>{item.label}</label>
             </div>
+          ))}
+        </div>
 
-            <div className="flex flex-col">
-              <label className="text-sm text-white mb-2 ml-3">Category</label>
-              <input
-                type="text"
-                className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
-                placeholder="Strength, Cardio, ..."
-                value={category}
-                onChange={(e) => {setCategory(e.target.value)}}
-                required
-              />
-            </div>
+        <div className="flex flex-col">
+          <label className="text-sm text-white mb-2 ml-3">Description</label>
+          <textarea
+            className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
 
-            <div className="flex flex-col">
-              <label className="text-sm text-white mb-2 ml-3">Body part</label>
-              <input
-                type="text"
-                className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
-                placeholder="Chest, Legs, ..."
-                value={bodyPart}
-                onChange={(e) => {setBodyPart(e.target.value)}}
-                required
-              />
-            </div>
+        <div className="flex flex-col">
+          <label className="text-sm text-white mb-2 ml-3">Common Mistakes</label>
+          <textarea
+            className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
+            value={commonMistakes}
+            onChange={(e) => setCommonMistakes(e.target.value)}
+          />
+        </div>
 
-            <div className='flex items-center justify-around'>
-              {
-                  difficultyList.map(item => {
-                      return <div className="flex flex-row gap-2">
-                          <input
-                              name='difficulty'
-                              type='radio'
-                              value={item.value}
-                              id={item.value}
-                              checked={difficulty === item.value}
-                              onChange={e => {
-                                  setDifficulty(e.target.value)
-                              }}
-                          />
-                          <label htmlFor={item.value}>{item.label}</label>
-                      </div>
-                  })
-              }
-            </div>
-
-            <div className="flex flex-col">
-              <label className="text-sm text-white mb-2 ml-3">Description</label>
-              <textarea
-                className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
-                placeholder=""
-                value={description}
-                onChange={(e) => {setDescription(e.target.value)}}
-                required
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <label className="text-sm text-white mb-2 ml-3">Common Mistakes</label>
-              <textarea
-                className="rounded-xl px-4 py-3 bg-white/20 text-white placeholder-white/70 focus:outline-none focus:ring-2 focus:ring-white focus:bg-white/30 transition"
-                placeholder=""
-                value={commonMistakes}
-                onChange={(e) => {setCommonMistakes(e.target.value)}}
-                required
-              />
-            </div>
-
-            <div className="flex flex-col">
-                <label className="text-sm text-white mb-2 ml-3">Upload Image</label>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => { 
-                        if (e.target.files && e.target.files.length > 0) {
-                            setImageFile(e.target.files[0]);
-                        }
-                    }}
-                    className="text-white"
-                    required
-                />
-            </div>
-
-          <button
-            type="submit"
-            className="bg-white text-black font-semibold py-3 rounded-xl hover:bg-gray-200 transition mt-4"
-            onClick={()=>{
-                
+        <div className="flex flex-col">
+          <label className="text-sm text-white mb-2 ml-3">Upload Image</label>
+          <FilePond
+            files={imageFile ? [imageFile] : []}
+            onupdatefiles={(fileItems) => {
+              const file = fileItems[0]?.file;
+              setImageFile(file || null);
             }}
-          >
-            Save
-          </button>
-        </form>
+            allowMultiple={false}
+            acceptedFileTypes={['image/*']}
+            labelIdle='اسحب الصورة هنا أو <span class="filepond--label-action">اختر من الجهاز</span>'
+          />
+        </div>
 
+        <button
+          type="submit"
+          className="bg-white text-black font-semibold py-3 rounded-xl hover:bg-gray-200 transition mt-4"
+        >
+          Save
+        </button>
+      </form>
     </div>
-  )
+  );
 }
