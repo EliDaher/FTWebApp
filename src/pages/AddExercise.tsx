2f1e525d3e1b011f7faebf3232f17cb2048565cd
EliDaher <eeli56315@gmail.com>
2025-05-03 10:01:29 +0300

    remove capture

--- src/pages/AddExercise.tsx
+++ src/pages/AddExercise.tsx
@@ -148,7 +148,6 @@ export default function Exercise() {
                 <input
                     type="file"
                     accept="image/*"
-                    capture="environment"
                     onChange={(e) => { 
                         if (e.target.files && e.target.files.length > 0) {
                             setImageFile(e.target.files[0]);
