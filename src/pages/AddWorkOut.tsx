19d570b10fedd846a139da9aaccb51dc074a8730
EliDaher <eeli56315@gmail.com>
2025-05-09 16:10:58 +0300

    fix

--- src/pages/AddWorkOut.tsx
+++ src/pages/AddWorkOut.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import { WorkOut, WorkOutExercise } from '../types/workout' 
 import axios from 'axios'
