80a7b759e3e4ee772aa935ceae9050d6966c5021
elidaher <eeli56315@gmail.com>
2025-06-12 14:05:30 +0300

    no logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from "../context/AuthContext";
 import { FaBars, FaDumbbell, FaHome, FaList, FaRunning, FaSignOutAlt, FaTimes, FaUsers } from 'react-icons/fa';
-import logo from '../assets/exerciseImages/icon.png';
 
 export default function Navbar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
